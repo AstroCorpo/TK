1f42abf72fe333afcb3fcefd3f6f653ee8170306
AstroCorpo <pietrek2911@gmail.com>
2024-10-31 17:14:10 +0100

    Added equal pattern

--- lab1/main.py
+++ lab1/main.py
@@ -81,10 +81,12 @@ class Scanner(Lexer):
 
     GREATEROREQUAL = r'>='
 
+    EQUAL = r'=='
+
 if __name__ == '__main__':
 
     try:
-        filename = sys.argv[1] if len(sys.argv) > 1 else "lab1/example.txt"
+        filename = sys.argv[1] if len(sys.argv) > 1 else "lab1/example_full.txt"
         file = open(filename, "r")
     except IOError:
         print("Cannot open {0} file".format(filename))
